c98ae18d38a9fb5ea54e4f5eae035ec57b45f79d
Metta <Chittibabu.Metta@chartindustries.com>
2020-01-11 11:28:05 +0530

    add bootstrap

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  Values: any;
+  Url: string;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.Url = environment.baseUrl;
+
+    this.httpClient.get(this.Url + '/values')
+      .subscribe(response => {
+        this.Values = response;
+      }, error => {
+        console.log('Error..');
+      });
+  }
+
+}
